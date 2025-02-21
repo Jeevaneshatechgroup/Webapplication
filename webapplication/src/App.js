cd9bf08729321498208fabdceab2998e03719597
jeevaneshatechgroup <jeevan@eshatechgroup.com>
2025-02-21 11:26:46 +0530

    update cost

--- webapplication/src/App.js
+++ webapplication/src/App.js
@@ -3,7 +3,6 @@ import RocketLaunches from "../src/component/RocketLaunches";
 function App() { 
   return ( 
     <div> 
-      <h1>API Integration Assignment</h1> 
       <RocketLaunches /> 
     </div> 
   ); 
--- webapplication/src/component/RocketLaunches.js
+++ webapplication/src/component/RocketLaunches.js
@@ -1,35 +1,33 @@
 import React, { useEffect, useState } from 'react'
 import Container from 'react-bootstrap/Container'
-import Button from 'react-bootstrap/Button'
 
 const RocketLaunches = () => {
   
-  const[data,setdata]=useState([])
+  const[data,setData]=useState([])
 
   useEffect(()=>{
     fetch("https://api.spacexdata.com/v3/rockets")
     .then(res=>res.json())
-    .then(data=>setdata(data));
+    .then(data=>setData(data));
    
     
   },[])
   return (
     <div>
       <Container>
-      <h1>API Integration</h1>
-        { data.map((rocket , index)=>(
-          <div className="card">
-         <h1>{rocket.company}</h1>
-          <p>{rocket.rocket_name}</p>
-          <p>{rocket.description}</p> 
-        </div>
-        )
-        
-      )
+        <h1>API Integration</h1>
+      {
+      data.map((rockets , index)=>(
+        <div className='card'>
+      <h1>{rockets.company}</h1>
+      <p>{rockets.rocket_name}</p>
+      <p>{rockets.cost_per_launch}</p>
+      <p>{rockets.description}</p>
+      </div>
+      ))
 
-        }
-        <Button onClick={() => window.location.reload()} variant="primary" style={{justifyContent:'center'}}>Refresh</Button>
-        </Container>
+      }
+      </Container>
         
     </div>
   )
